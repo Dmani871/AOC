a652cdd6c57a69f552af497815619b10904d882e
dmanibarnett <dmani.barnett@gmail.com>
2021-12-14 07:02:17 +0000

    Completed C1-02-2021-Python

--- 2021/Day1/C2.py
+++ 2021/Day1/C2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 def window_increasese():
-    df = pd.read_csv('input.txt', delimiter="\n", names=["depths"])
+    df = pd.read_csv('input-simple.txt', delimiter="\n", names=["depths"])
     windows=np.convolve(df["depths"], np.ones(3, dtype=int), 'valid')
     diffs = np.ediff1d(windows, to_begin=0)
     return (0 < diffs).sum()
