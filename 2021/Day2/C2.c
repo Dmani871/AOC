17ef52ed8f6e03475d879ccb873fa87a3d5f24e1
dmanibarnett <dmani.barnett@gmail.com>
2021-12-15 00:26:05 +0000

    Completed C2-02-2021-C

---
+++ 2021/Day2/C2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int compareString(char*cmd,char*instruction){
+    int index=0;
+    while(cmd[index]!='\0'){
+        if(cmd[index]!=instruction[index]){
+            return -1;
+        }
+        index++;
+    }
+    return 0;
+}
+int main()
+{
+    FILE *fileHandleIn = fopen("input.txt", "r");
+    char buffer[20];
+    int units=0;
+    int horizontal=0;
+    int depth=0;
+    int aim=0;
+    while ((fscanf(fileHandleIn, "%s", buffer)==1) && 
+    (fscanf(fileHandleIn, "%d", &units)==1)){
+       if(compareString("forward",buffer)==0){
+           horizontal+=units;
+           depth+=aim*units;
+       }else if (compareString("down",buffer)==0)
+       {
+           aim+=units;
+       }else if (compareString("up",buffer)==0)
+       {
+           aim-=units;
+       }
+    }
+    printf("%d \n",horizontal*depth);
+}
+
