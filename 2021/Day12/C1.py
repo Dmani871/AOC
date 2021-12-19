2123986be3dd836a12d5bda01de78aed46054d8a
dmanibarnett <dmani.barnett@gmail.com>
2021-12-19 19:52:26 +0000

    Completed C1-12-2021-Python

---
+++ 2021/Day12/C1.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+
+def remove_dead_ends(start, connection_map, visited):
+    if start == 'end':
+        return 1
+    paths = 0
+    visited.append(start)
+    start_length=len(visited)
+    for cave in connection_map[start]:
+        if (cave.islower() and cave not in visited) or cave.isupper():
+            paths += remove_dead_ends(cave, connection_map, visited)
+        visited=visited[:start_length]
+    return paths
+
+
+def main():
+    connection_map = defaultdict(list)
+    with open('input.txt', 'r') as infile:
+        for connection in infile:
+            cave1, cave2 = connection.strip().split("-")
+            if cave2 != "start":
+                connection_map[cave1].append(cave2)
+            if cave1 != "start":
+                connection_map[cave2].append(cave1)
+        del connection_map['end']
+        return remove_dead_ends('start', connection_map, [])
+
+
+if __name__ == '__main__':
+    print(main())
